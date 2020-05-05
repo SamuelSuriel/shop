980508fc1bb92cc4f52b124d3a4400804d0603aa
Samuel Ramirez <34385530+SamuelSuriel@users.noreply.github.com>
2020-05-05 16:43:55 -0400

    Add user management

--- shop.Web/Data/Entities/User.cs
+++ shop.Web/Data/Entities/User.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNetCore.Identity;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class User : IdentityUser
     {
@@ -20,7 +21,19 @@
 
         public City City { get; set; }
 
+        [Display(Name = "Phone Number")]
+        public override string PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
+
+        
         [Display(Name = "Full Name")]
         public string FullName { get {return $"{this.FirstName} {this.LastName}"; }  }
+
+        [Display(Name = "Email Confirmed")]
+        public override bool EmailConfirmed { get => base.EmailConfirmed; set => base.EmailConfirmed = value; }
+
+        [NotMapped]
+        [Display(Name = "Is Admin?")]
+        public bool IsAdmin { get; set; }
+
     }
 }
