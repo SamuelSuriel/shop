b9caf594e2a3d549694684388d43bdb33ec2457b
Samuel Ramirez <34385530+SamuelSuriel@users.noreply.github.com>
2020-05-06 19:02:43 -0400

    Add the Master Detail Page and the Menu Page to the App

--- shop.UIForms/shop.UIForms/ViewModels/MainViewModel.cs
+++ shop.UIForms/shop.UIForms/ViewModels/MainViewModel.cs
@@ -1,10 +1,16 @@
-﻿using shop.Common.Models;
-
-namespace shop.UIForms.ViewModels
+﻿namespace shop.UIForms.ViewModels
 {
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using Common.Models;
+
+
     public class MainViewModel
     {
         private static MainViewModel instance;
+
+        public ObservableCollection<MenuItemViewModel> Menus { get; set; }
 
         public TokenResponse Token { get; set; }
 
@@ -16,7 +22,43 @@ namespace shop.UIForms.ViewModels
         public MainViewModel()
         {
             instance = this;
+            this.LoadMenus();
         }
+
+        private void LoadMenus()
+        {
+            var menus = new List<Menu>
+    {
+        new Menu
+        {
+            Icon = "ic_info",
+            PageName = "AboutPage",
+            Title = "About"
+        },
+
+        new Menu
+        {
+            Icon = "ic_phonelink_setup",
+            PageName = "SetupPage",
+            Title = "Setup"
+        },
+
+        new Menu
+        {
+            Icon = "ic_exit_to_app",
+            PageName = "LoginPage",
+            Title = "Close session"
+        }
+    };
+
+            this.Menus = new ObservableCollection<MenuItemViewModel>(menus.Select(m => new MenuItemViewModel
+            {
+                Icon = m.Icon,
+                PageName = m.PageName,
+                Title = m.Title
+            }).ToList());
+        }
+
 
         public static MainViewModel GetInstance()
         {
